998fbd1d5f8b0f6b8cab9b269295716e627e5d2a
devlrakib <devrakib5@gmail.com>
2024-03-23 09:09:12 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Hello Javascript Practice');
+document.getElementById('title').innerHTML = 'Hello World';
+console.log('Hello Js Programmer');
+
+let sum = 150+40;
+console.log(sum);
+if (sum > 100){
+    console.log('Expensive');
+}
+else{
+    console.log('cheap');
+}
+
+
+function sayHello (name){
+    console.log(`Hello ${name}`);
+}
+sayHello('Rakib')
